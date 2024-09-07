5596f935684a47239b96658b30991a1e05b101d4
none <none>
2024-09-07 23:12:29 +0930

    refactor and bug fix

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
 import sys
 
-# additional things to ask:
-# - how is the program's width calculated
-#   It includes whitespace (not newlines)
-# - how do you break size ties
-#   look at the bottom row
-# - which order to add real symbols
-# - do real symbols depend on other shapes
-#   e.g.  AA
-#         A A
-#          AA
-#   this is 2 shapes, are there still real symbols here?
-
 class Shape:
-    def __init__(self, contents, row, col, visited):
+    def __init__(self, grid, row, col, visited):
         # the symbol of the shape
-        self.symbol = contents[row][col]
+        self.symbol = grid[row][col]
 
         # Breadth first traversal
         next = []
@@ -29,12 +17,12 @@ class Shape:
             l = [0, 1, 0, -1]
             for i in range(4):
                 next_y = y + l[i]
-                if not (0 <= next_y < len(contents)):
+                if not (0 <= next_y < len(grid)):
                     continue
                 next_x = x + l[(i + 1) % 4]
-                if not (0 <= next_x < len(contents[next_y])):
+                if not (0 <= next_x < len(grid[next_y])):
                     continue
-                if contents[next_y][next_x] == self.symbol and (next_y, next_x) not in visited:
+                if grid[next_y][next_x] == self.symbol and (next_y, next_x) not in visited:
                     next.append((next_y, next_x))
                     visited.append((next_y, next_x))
             base += 1
@@ -46,23 +34,24 @@ class Shape:
         min_y = min(next, key=lambda pos: pos[0])[0]
         min_x = min(next, key=lambda pos: pos[1])[1]
         max_x = max(next, key=lambda pos: pos[0])[0]
-        self.offset = (min_y, min_x)
+        self.y = min_y
+        self.x = min_x
         self.width = max_x - min_x + 1
 
     def ordering(self):
         # assume positions is sorted
         max_y = self.positions[-1][0]
-        # order by number of tiles
-        # lowest position
-        # count of tiles at that position
+        # order by number of tiles,
+        # lowest position,
+        # count of tiles at that height
         return (self.count, max_y, [y for y, x in self.positions].count(max_y))
 
     def print(self):
-        y, x = self.offset
+        y, x = self.y, self.x
         for pos in self.positions:
             while y < pos[0]:
                 print()
-                x = self.offset[1]
+                x = self.x
                 y += 1
             while x < pos[1]:
                 print(' ', end='')
@@ -96,6 +85,62 @@ def find_real(stiles, symbol):
     return [real for real in stiles if real_below(real)]
 
 
+def parse_shapes(grid):
+    shapes = []
+    visited = []
+    for row, line in enumerate(grid):
+        for col, symbol in enumerate(line):
+            if not symbol.isspace() and (row, col) not in visited:
+                shapes.append(Shape(grid, row, col, visited))
+    return shapes
+
+
+# assume ordered
+def calculate_offsets(shapes):
+    acc = 0
+    for shape in shapes:
+        shape.buffer_x_offset = acc
+        acc += shape.width
+
+
+def make_rectangle(shapes, width):
+    # put it all into a list
+    buffer = [(tile[0], tile[1] - shape.x + shape.buffer_x_offset, shape.symbol) for shape in shapes for tile in shape.positions]
+    real = []
+    symbols = {shape.symbol for shape in shapes}
+    for symbol in symbols:
+        only_one_symbol = [tile for tile in buffer if tile[2] == symbol]
+        real += find_real(only_one_symbol, symbol)
+    real.sort(reverse=True) # <- is this the correct ordering?
+
+    # start building the final rectangle
+    # add the real symbols
+    final_rect = [tile[2] for tile in real]
+    # add the imaginary symbols
+    final_rect += [tile[2] for tile in buffer if tile not in real]
+
+    # run the copy rule
+    copy_rule = [final_rect[x + width + 1] for x in range(len(real)) if x + width + 1 < len(final_rect)]
+    final_rect += copy_rule
+    return final_rect
+
+
+def one_iteration(grid):
+    # The width includes whitespace
+    width = max([len(l) for l in grid])
+
+    shapes = parse_shapes(grid)
+
+    shapes.sort(key=lambda shape: shape.ordering(), reverse=True)
+    print("Shapes:")
+    for shape in shapes:
+        shape.print()
+
+    calculate_offsets(shapes)
+
+    return make_rectangle(shapes, width), width
+
+
 def interpret(file):
     # read the input and clean up newlines
     with open(file, 'r') as f:
@@ -105,56 +150,12 @@ def interpret(file):
     for line in content:
         print("  " + "".join(line))
 
-    # The width includes whitespace
-    width = max([len(l) for l in content])
-
-    # parse the shapes
-    shapes = []
-    visited = []
-    symbols = set()
-    for row, line in enumerate(content):
-        for col, symbol in enumerate(line):
-            if not symbol.isspace() and (row, col) not in visited:
-                shapes.append(Shape(content, row, col, visited))
-                symbols.add(symbol)
-
-    # doing tie breaking by what was parsed first
-    # i.e. left to right, top to bottom
-    shapes.sort(key=lambda shape: shape.ordering(), reverse=True)
-    print("Shapes:")
-    for shape in shapes:
-        shape.print()
-
-    # find the buffer offsets
-    # and add them to symbol list
-    same_symbol = {}
-    acc = 0
-    for shape in shapes:
-        shape.buffer_offset = acc
-        if shape.symbol not in same_symbol:
-            same_symbol[shape.symbol] = []
-        same_symbol[shape.symbol].append(shape)
-        acc += shape.width
-
-    # put it all into a list
-    buffer = [(tile[0] - shape.offset[0], tile[0] - shape.offset[1] + shape.buffer_offset, shape.symbol) for shape in shapes for tile in shape.positions]
-    real = []
-    for symbol in symbols:
-        only_one_symbol = [tile for tile in buffer if tile[2] == symbol]
-        real += find_real(only_one_symbol, symbol)
-    real.sort(reverse=True)
-
-    # start building the final rectangle
-    final_rect = [tile[2] for tile in real]
-    final_rect += [tile[2] for tile in buffer if tile not in real]
-
-    # do the final thingo
-    bonus = [final_rect[x + width + 1] for x in range(len(real)) if x + width + 1 < len(final_rect)]
-
-    final_rect += bonus
+    final_rect, width = one_iteration(content)
 
     if len(final_rect) % width == 0:
-        pass
+        print("continue")
+    else:
+        print("halt")
 
 
 if __name__ == '__main__':
